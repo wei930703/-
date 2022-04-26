671fb94ff8d71642850275c092fe5496b2022413
wei930703 <90673390+wei930703@users.noreply.github.com>
2022-04-26 13:27:55 +0800

    Update triangle.c

--- triangle.c
+++ triangle.c
@@ -6,10 +6,10 @@ main () {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n-i; j++) {
-            if (i % 2 ==0){
-                printf("+");
-            } else {
+            if (i % 2 == 0) {
                 printf("*");
+            } else {
+                printf("+");
             }
         }
         printf("\n");
